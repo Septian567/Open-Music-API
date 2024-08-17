c1219ba7e75b2e9b6245cd7d7d7c78fb155c2c02
Septian Dika Maulana <99572215+Septian567@users.noreply.github.com>
2024-08-17 22:21:40 +0700

    Delete open-music-api-v1 directory

--- open-music-api-v1/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import globals from 'globals';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import pluginJs from '@eslint/js';
-
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-];
